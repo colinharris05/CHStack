41aac005998dd46c1635f10af8df8687b3226603
Devon Harris <dbharris2@gmail.com>
2016-04-02 17:24:16 -0700

    [chstack] Add peek test for 2 elements

--- CHStackPeekTests.c
+++ CHStackPeekTests.c
@@ -20,9 +20,22 @@ void TestPeekWithOneElement(CuTest *tc) {
   CuAssertIntEquals(tc, expected, *actual);
 }
 
+void TestPeekWithTwoElements(CuTest *tc) {
+  CHStack *stack = newCHStack(5);
+  int i = 5;
+  push(stack, &i);
+  int j = 7;
+  push (stack, &j);
+
+  int *actual = (int *)peek(stack);
+  int expected = 7;
+  CuAssertIntEquals(tc, expected, *actual);
+}
+
 CuSuite* CHStackPeekGetSuite() {
   CuSuite* suite = CuSuiteNew();
   SUITE_ADD_TEST(suite, TestPeekWithZeroElements);
   SUITE_ADD_TEST(suite, TestPeekWithOneElement);
+  SUITE_ADD_TEST(suite, TestPeekWithTwoElements);
   return suite;
 }
