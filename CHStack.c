d85f768d0250089664a81788bb53d7f0de070be7
= <colinharris05@gmail.com>
2016-04-07 07:29:58 -0500

    Rely on linked list size

--- CHStack.c
+++ CHStack.c
@@ -6,15 +6,12 @@
 struct _CHStack {
   CHLinkedList *elements;
   int maxElems;
-  int size;
 };
 
 CHStack *chstack_new(const unsigned int maxElems) {
   CHStack *stack = malloc(sizeof(CHStack));
   stack->maxElems = maxElems;
-  // stack->elements = malloc(sizeof(void *) * maxElems); // Default to size 10
   stack->elements = chlinkedlist_new();
-  stack->size = 0;
   return stack;
 }
 
@@ -23,51 +20,26 @@ void chstack_free(CHStack *stack) {
   stack = NULL;
 }
 
-void **doubleArraySize(void **elements, const int size) {
-  void **newArray = malloc(sizeof(void *) * 2*size);
-  int i;
-  for (i = 0; i < size; i++) {
-    newArray[i] = elements[i];
-  }
-  return newArray;
-}
-
-// Used to assure the stack has enough space allocated
-void checkSize(CHStack *stack) {
-  if ((stack->size + 1) == stack->maxElems) {
-    printf(" ***--Doubling array--***\n");
-    stack->elements = doubleArraySize(stack->elements, stack->maxElems);
-  }
-}
-
 void *chstack_peek(CHStack *stack) {
-  if (stack->size > 0) {
-    return chlinkedlist_get(stack->elements, stack->size - 1);
+  if (chstack_size(stack) > 0) {
+    return chlinkedlist_get(stack->elements, chstack_size(stack) - 1);
   } else {
     return NULL;
   }
 }
 
 void *chstack_pop(CHStack *stack) {
-  if (stack->size == 0) {
+  if (chstack_size(stack) == 0) {
     return NULL;
   }
   void *result = chlinkedlist_pop(stack->elements);
-  stack->size--;
   return result;
 }
 
 void chstack_push(CHStack *stack, void *p) {
-  if (stack->size == 0) {
-    chlinkedlist_append(stack->elements, p);
-    stack->size = 1;
-  } else {
-    checkSize(stack);
-    chlinkedlist_append(stack->elements, p);
-    stack->size++;
-  }
+  chlinkedlist_append(stack->elements, p);  
 }
 
 int chstack_size(CHStack *stack) {
-  return stack->size;
+  return chlinkedlist_size(stack->elements);
 }
